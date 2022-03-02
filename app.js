f59b981899e0a82659d1f1b9205dc9074845b776
William <william98m@gmail.com>
2022-03-02 08:40:41 -0500

    removed cors

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-const cors = require('cors')
+// const cors = require('cors')
 const port = process.env.PORT || 3000
 require('dotenv/config')
 
-app.use(
-    cors({
-        origin: "http://localhost:3000/",
-}))
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000/",
+// }))
 app.use(bodyParser.json())
 
 //import routes
